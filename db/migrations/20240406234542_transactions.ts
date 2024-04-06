021f9f8108daa09a0f2f31d6ff28abf3bbab240b
PedroDressler <pedro2003dressler@gmail.com>
2024-04-06 20:50:33 -0300

    new database

---
+++ db/migrations/20240406234542_transactions.ts
@@ -0,0 +1,15 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('transactions', (table) => {
+    table.uuid('id').primary()
+    table.uuid('session_id')
+    table.string('title').notNullable()
+    table.timestamp('created_at').notNullable()
+    table.decimal('amount', 10, 2).notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('transactions')
+}
